e9035f84c5d1ddad30f3f9629128ba71ecbd7f20
Useit14 <useit14@inbox.ru>
2022-08-05 18:23:27 +0300

    Fix bags

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-let magicalNumber = 15;
-let number = prompt("Введите число", "25");
+let magicalNumber = Math.round(Math.random() * 100);
 
 const isNumber = function (num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
 const game = function (magicalNumber) {
+  const gameOver = function () {
+    alert("Игра окончена");
+  };
+
+  const getRandomNumber = function () {
+    return Math.round(Math.random() * 100);
+  };
+
   const ask = function () {
-    let number = prompt("Введите число");
-    if (isNumber(number) && number != null) {
+    let number = prompt("Введите число", "25");
+    if (number == null) {
+      return null;
+    } else if (isNumber(number)) {
       return number;
     } else {
-      ask();
+      return ask();
     }
   };
 
@@ -19,18 +28,35 @@ const game = function (magicalNumber) {
     if (number == magicalNumber) {
       let answer = confirm("Поздравляю, Вы угадали!!!, Хотите сыграть еще?");
       if (answer == true) {
-        isWin(ask());
+        game(getRandomNumber());
+      } else {
+        gameOver();
       }
     } else if (number > magicalNumber) {
       alert("Загаданное число меньше");
-      isWin(ask());
+      let number = ask();
+      if (number == null) {
+        gameOver();
+      } else {
+        isWin(number);
+      }
     } else if (number < magicalNumber) {
       alert("Загаданное число больше");
-      isWin(ask());
+      let number = ask();
+      if (number == null) {
+        gameOver();
+      } else {
+        isWin(number);
+      }
     }
   };
 
-  isWin(number);
+  let number = ask();
+  if (number == null) {
+    gameOver();
+  } else {
+    isWin(number);
+  }
 };
 
 game(magicalNumber);
